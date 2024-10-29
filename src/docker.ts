8479e9040e4f1f60584fa3c9e88b3b03128cb069
Fedor Dikarev <fedor.dikarev@gmail.com>
2024-10-29 22:03:00 +0100

    add retries for 502
    Signed-off-by: Fedor Dikarev <fedor.dikarev@gmail.com>

--- src/docker.ts
+++ src/docker.ts
@@ -3,11 +3,11 @@ import * as core from '@actions/core';
 
 import {Docker} from '@docker/actions-toolkit/lib/docker/docker';
 
-export async function login(registry: string, username: string, password: string, ecr: string): Promise<void> {
+export async function login(registry: string, username: string, password: string, ecr: string, attempts: number): Promise<void> {
   if (/true/i.test(ecr) || (ecr == 'auto' && aws.isECR(registry))) {
     await loginECR(registry, username, password);
   } else {
-    await loginStandard(registry, username, password);
+    await loginStandard(registry, username, password, attempts);
   }
 }
 
@@ -21,7 +21,7 @@ export async function logout(registry: string): Promise<void> {
   });
 }
 
-export async function loginStandard(registry: string, username: string, password: string): Promise<void> {
+export async function loginStandard(registry: string, username: string, password: string, attempts: number): Promise<void> {
   if (!username && !password) {
     throw new Error('Username and password required');
   }
@@ -41,16 +41,29 @@ export async function loginStandard(registry: string, username: string, password
   } else {
     core.info(`Logging into Docker Hub...`);
   }
-  await Docker.getExecOutput(loginArgs, {
-    ignoreReturnCode: true,
-    silent: true,
-    input: Buffer.from(password)
-  }).then(res => {
-    if (res.stderr.length > 0 && res.exitCode != 0) {
-      throw new Error(res.stderr.trim());
+  let attempt: number = 1
+  let succeeded: boolean = false
+  for (let attempt = 1; (attempt <= attempts) && (!succeeded); attempt++) {
+    await Docker.getExecOutput(loginArgs, {
+      ignoreReturnCode: true,
+      silent: true,
+      input: Buffer.from(password)
+    }).then(res => {
+      if (res.stderr.length > 0 && res.exitCode != 0) {
+        let isRetriable: boolean
+        isRetriable = res.stderr.endsWith("502 Bad Gateway")
+        if (!isRetriable || (attempt >= attempts) {
+          throw new Error(res.stderr.trim());
+        }
+      } else {
+        core.info(`Login Succeeded!`);
+        succeeded = true;
+      }
+    });
+    if ((attempt < attempts) && !succeeded) {
+      await new Promise(r => setTimeout(r, 10000))
     }
-    core.info(`Login Succeeded!`);
-  });
+  }
 }
 
 export async function loginECR(registry: string, username: string, password: string): Promise<void> {
