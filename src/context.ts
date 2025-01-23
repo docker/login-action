2bc89718bcfd01231178fc0d337d68f2e05a7cf3
Fedor Dikarev <fedor.dikarev@gmail.com>
2025-01-23 02:27:56 +0100

    add args for retries
    Signed-off-by: Fedor Dikarev <fedor.dikarev@gmail.com>

--- src/context.ts
+++ src/context.ts
@@ -6,6 +6,9 @@ export interface Inputs {
   password: string;
   ecr: string;
   logout: boolean;
+  http_errors_to_retry: string[];
+  max_attempts: number;
+  retry_timeout: number;
 }
 
 export function getInputs(): Inputs {
@@ -14,6 +17,9 @@ export function getInputs(): Inputs {
     username: core.getInput('username'),
     password: core.getInput('password'),
     ecr: core.getInput('ecr'),
-    logout: core.getBooleanInput('logout')
+    logout: core.getBooleanInput('logout'),
+    http_errors_to_retry: core.getInput('http_errors_to_retry').split(','),
+    max_attempts: Number.parseInt(core.getInput('max_attempts')),
+    retry_timeout: Number.parseInt(core.getInput('retry_timeout'))
   };
 }
